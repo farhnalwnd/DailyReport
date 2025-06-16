147b05e290a4b4e05ce4cb5ef08a797334e0c938
aderusmana <aderusmana777@gmail.com>
2025-06-16 09:54:26 +0700

    Add ExportDoc and ExportDocDetail models with relationships and table definitions

--- app/Models/QAD/Export/ExportDoc.php
+++ app/Models/QAD/Export/ExportDoc.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ExportDoc extends Model
 {
     use HasFactory;
+    protected $table = 'export_docs';
+    protected $guarded = ['id'];
+
+
+    public function details()
+    {
+        return $this->hasMany(ExportDocDetail::class, 'sod_nbr', 'so_nbr');
+    }
 }
--- app/Models/QAD/Export/ExportDocDetail.php
+++ app/Models/QAD/Export/ExportDocDetail.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ExportDocDetail extends Model
 {
     use HasFactory;
+    protected $table = 'export_doc_details';
+    protected $guarded = ['id'];
+
+    public function exportDoc()
+    {
+        return $this->belongsTo(ExportDoc::class, 'sod_nbr', 'so_nbr');
+    }
 }
