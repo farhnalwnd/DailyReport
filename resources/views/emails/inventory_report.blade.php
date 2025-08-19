36ef88d82aab2d30f576e65e70950cc847867d56
farhan alwanda <raja.farhan.alwanda@gmail.com>
2025-08-19 13:29:42 +0700

    fix: rapikan tampilan email

--- resources/views/emails/inventory_report.blade.php
+++ resources/views/emails/inventory_report.blade.php
@@ -20,8 +20,7 @@
                                         Laporan Stok Barang
                                     </td>
                                     <td align="right" width="80">
-                                        <img src="{{ asset('assets/images/logo/sinarmeadow.png') }}" alt="Logo" width="60"
-                                            style="display:block;">
+                                        <img src="{{ $message->embed(public_path('assets/images/logo/sinarmeadow.png')) }}" alt="Logo Sinar Meadow" width="60" style="display:block;">
                                     </td>
                                 </tr>
                             </table>
@@ -35,21 +34,25 @@
                     <tr>
                         <td style="padding:30px 30px 20px 30px; color:#333; font-size:16px;">
                             <p>Yth. Bapak/Ibu {{ $user->name}},</p>
-                            <p>Berikut kami lampirkan laporan stok barang terbaru dalam bentuk file Excel.</p>
+                            <p>Berikut kami lampirkan laporan stok barang terbaru dalam bentuk file Excel, pertanggal {{ now()->format('d-m-Y') }}</p>
                             <p>Silakan unduh attachment pada email ini untuk melihat detail laporan.</p>
                         </td>
                     </tr>
+
+                    <tr>
+                        <td style="border-bottom:1px solid #e0e0e0;"></td>
+                    </tr>
+
                     <!-- Footer -->
                     <tr>
                         <td style="padding:20px 30px 10px 30px;">
                             <table width="100%">
                                 <tr>
-                                    <td align="center" style="font-size:13px;color:#888;">
+                                    <td align="center" style="font-size:13px;color:#888; padding: 0 30px 10px 30px;">
                                         &copy; {{ date('Y') }} PT. Sinar Meadow International Indonesia. All rights reserved.<br>
-                                        Kawasan Industri Pulogadung, Jl. Pulo Ayang, Jatinegara, Kec. Cakung,<br>Kota Jakarta
-                                        Timur, Daerah Khusus Ibukota Jakarta<br>
-                                        <a href="mailto:info@perusahaan.com"
-                                            style="color:#888;text-decoration:none;">info@perusahaan.com</a>
+                                        <span style="display:inline-block; padding:10px 20px; background:#f4f4f4; border-radius:6px; margin-top:8px;">
+                                            Kawasan Industri Pulogadung, Jl. Pulo Ayang, Jatinegara, Kec. Cakung,<br>Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta
+                                        </span><br>
                                     </td>
                                 </tr>
                             </table>
